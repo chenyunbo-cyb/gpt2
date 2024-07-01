37ad00a036a46df7f60d76cff0bbb780fb8deb7b
ChenYunbo <13920883+chenyunbo404@user.noreply.gitee.com>
2024-07-01 15:35:14 +0800

    Sample Loop 测试eval()

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import math
 
 import torch
 import torch.nn as nn
@@ -109,6 +110,28 @@ class GPT(nn.Module):
         # 线性层，最终的分类器
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias= False)
     
+    def forward(self, idx, targets = None):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T<=self.config.block_size, f"T:{T} is bigger than block_size:{self.config.block_size}"
+
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)  #shape T
+        pos_emb = self.transformer.wpe(pos)  # shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx)  # shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+
+        for block in self.transformer.h:
+            x = block(x)
+        
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)  # shape (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
+
+
+    
     @classmethod  
     def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from huggingface"""
@@ -167,5 +190,44 @@ class GPT(nn.Module):
 
 
 # -----------------------------------------------------------------------------------------
+num_return_sequences = 5
+max_length = 30
+device = 'cuda'
+
 model = GPT.from_pretrained('gpt2')
+model.eval()
+model.to(device)
 print(" Yes! First Success! ")
+
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tockens = enc.encode("Hello, I'm a language model,")
+tockens = torch.tensor(tockens, dtype=torch.long) #(8, )
+tockens = tockens.unsqueeze(0).repeat(num_return_sequences, 1) #(5, 8)
+x = tockens.to(device)
+
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+
+    with torch.no_grad():
+        logits = model(x)[0]
+
+        logits = logits[:, -1, :]
+
+        probs = F.softmax(logits, dim=-1)
+
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+
+        ix = torch.multinomial(topk_probs, 1)
+
+        xcol = torch.gather(topk_indices, -1, ix)
+
+        x = torch.cat((x, xcol), dim=1)
+
+for i in range(num_return_sequences):
+    tockens = x[i, :max_length].tolist()
+    decoded = enc.decode(tockens)
+    print(">", decoded)
